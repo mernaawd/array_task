dd0cd9b940462b3cbcd6565b1297213ec9ed88cc
mernaawd <125801027+mernaawd@users.noreply.github.com>
2024-05-26 00:45:50 +0300

    Add files via upload

---
+++ main.js.js
@@ -0,0 +1,48 @@
+
+// // Task3.1
+// var arr= [];
+// for(i=0;i<3;i++){
+//    var z=parseFloat(prompt("Please enter your element"));
+//    arr.push(z);
+// }
+// console.log(arr);
+// function addaing(arr){
+// var m= arr[0]+arr[1]+arr[2];
+// return m;
+// console.log(m);
+// }
+// var n=addaing(arr);
+// document.write("<h1>the adding of your elements:"+n+"</h1>")
+// function multiplying(arr){
+//     var m= arr[0]*arr[1]*arr[2];
+//     return m;
+//     console.log(m);
+//     }
+//     var y=multiplying(arr);
+//     document.write("<h1>the multiplying of your elements:"+y+"</h1>")
+// function dividing(arr){
+//     var m= arr[0]/arr[1]/arr[2];
+//     return m;
+//     console.log(m);
+//     }
+//     var y=dividing(arr);
+//     document.write("<h1>the dividing of your elements:"+y+"</h1>")
+
+// Task3.2
+// var arr= [];
+// for(i=0;i<5;i++){
+//    var z=parseFloat(prompt("Please enter your element"));
+//    arr.push(z);
+// }
+// function asending(arr){
+//    return  arr.slice().sort(function(a,b) {
+//        return a - b;
+//     });
+// }
+// console.log(Dsending(arr));
+// function Dascending(arr){
+//    return  arr.slice().sort(function(a,b) {
+//        return b - a;
+//     });
+// }
+// console.log(asending(arr));
